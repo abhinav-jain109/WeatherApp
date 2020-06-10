fc2f308f9fb68ae2e8f6e32ef7ca304b4a888e25
Abhinav Jain <abhinav.jain@ns.nl>
2020-06-10 17:39:34 +0200

    adding validation for paramater in the unix script

--- startapp.sh
+++ startapp.sh
@@ -1,2 +1,8 @@
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Possible aurgment are local,test,acc,prod.Example- sh startapp.sh local"
+    exit 1
+fi
+echo
 mvn clean install -Denv=docker
 denv="$1" docker-compose up
